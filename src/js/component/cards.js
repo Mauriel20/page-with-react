c7fae5981a1083760a96798d046e15d1d89641df
Anmora96 <anmora.2396@gmail.com>
2021-02-04 22:04:36 +0000

    Ejercicio de ReactJs Terminado

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -1,88 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-//crear los objetos
-const animals = [
-	{ label: "Horse" },
-	{ label: "Turtle" },
-	{ label: "Elephant" },
-	{ label: "Monkey" }
-];
-//este cambio
-//hacer una variable para los objectos
-//hacer una function de map
-//se agrega donde se van a enviar
-
-export function Cards() {
+export function Cards(props) {
 	return (
-		<div className="card-deck">
-			<div className="card">
-				<img
-					className="card-img-top"
-					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
-					alt="Card image cap"
-				/>
-				<div className="card-body">
-					<h4 className="card-title">Card title</h4>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#!" className="btn btn-primary">
-						Go somewhere
-					</a>
-				</div>
-			</div>
-			<div className="card">
-				<img
-					className="card-img-top"
-					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
-					alt="Card image cap"
-				/>
-				<div className="card-body">
-					<h4 className="card-title">Card title</h4>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#!" className="btn btn-primary">
-						Go somewhere
-					</a>
-				</div>
-			</div>
-			<div className="card">
-				<img
-					className="card-img-top"
-					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
-					alt="Card image cap"
-				/>
-				<div className="card-body">
-					<h4 className="card-title">Card title</h4>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#!" className="btn btn-primary">
-						Go somewhere
-					</a>
-				</div>
-			</div>
-			<div className="card">
-				<img
-					className="card-img-top"
-					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
-					alt="Card image cap"
-				/>
-				<div className="card-body">
-					<h4 className="card-title">Card title</h4>
-					<p className="card-text">
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</p>
-					<a href="#!" className="btn btn-primary">
-						Go somewhere
-					</a>
-				</div>
+		<div className="card">
+			<img
+				className="card-img-top"
+				src={props.image}
+				alt="Card image cap"
+			/>
+			<div className="card-body">
+				<h4 className="card-title">{props.title}</h4>
+				<p className="card-text">{props.description}</p>
+				<a
+					href={props.buttonlink}
+					className="btn btn-primary text-light">
+					{props.buttonlabel}
+				</a>
 			</div>
 		</div>
 	);
 }
+
+Cards.propTypes = {
+	image: PropTypes.string,
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonlink: PropTypes.string,
+	buttonlabel: PropTypes.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -13,7 +13,32 @@ export function Home() {
 			<Navbar />
 			<div className="container">
 				<Jumbotron />
-				<Cards />
+				<div className="card-deck">
+					<Cards
+						title="Titulo"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+						image="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+						buttonlabel="Go Somewhere"
+					/>
+					<Cards
+						title="Titulo"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+						image="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+						buttonlabel="Go Somewhere"
+					/>
+					<Cards
+						title="Titulo"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+						image="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+						buttonlabel="Go Somewhere"
+					/>
+					<Cards
+						title="Titulo"
+						description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+						image="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+						buttonlabel="Go Somewhere"
+					/>
+				</div>
 			</div>
 			<Footer />
 		</div>
