3f55278196eeb21fb49f5c1a6b031d8cc1cf5480
Anmora96 <anmora.2396@gmail.com>
2021-02-04 19:03:18 +0000

    Ejercicio ReactJs

---
+++ src/js/component/cards.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+//crear los objetos
+const animals = [
+	{ label: "Horse" },
+	{ label: "Turtle" },
+	{ label: "Elephant" },
+	{ label: "Monkey" }
+];
+
+//hacer una variable para los objectos
+//hacer una function de map
+//se agrega donde se van a enviar
+
+export function Cards() {
+	return (
+		<div className="card-deck">
+			<div className="card">
+				<img
+					className="card-img-top"
+					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h4 className="card-title">Card title</h4>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#!" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card">
+				<img
+					className="card-img-top"
+					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h4 className="card-title">Card title</h4>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#!" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card">
+				<img
+					className="card-img-top"
+					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h4 className="card-title">Card title</h4>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#!" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card">
+				<img
+					className="card-img-top"
+					src="https://metalmagnitude.com/wp-content/uploads/2016/10/Trivium-Logo.png"
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h4 className="card-title">Card title</h4>
+					<p className="card-text">
+						Some quick example text to build on the card title and
+						make up the bulk of the card's content.
+					</p>
+					<a href="#!" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<div className="bottom bg-dark text-dark mt-3">
+			<footer className="blockquote-footer pt-2 pb-2 text-center">
+				Someone famous in <cite title="Source Title">Source Title</cite>
+			</footer>
+		</div>
+	);
+}
